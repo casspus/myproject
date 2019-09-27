3502ef3d05e2644737cb9c421a352b09f3cf8f91
KBelova <bcasspus@gmail.com>
2019-09-27 09:48:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+a = 5;
+
+console.log(a);
+
+// console.log(4/0);
+// console.log('string'*9);
+
+// let some;
+// console.log(some);
+
+// let persone = {
+// 	name: "John",
+// 	age: 25,
+// 	isMarried: false
+// };
+
+// console.log(persone["name"]);
+
+// let arr = ['green.png','red.jpg','blue.gif'];
+
+// console.log(arr[2]);
+
+// let answer = confirm('are you here?');
+// console.log(answer);
+
+// let answer = +prompt('Сколько вам лет?', '');
+// console.log(answer);
+
+let incr = 10,
+	decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log("2" == 2);
+
+let isChecked = true,
+	isClose = true;
+
+console.log(isClose && isChecked);	
