b9aeb864e239d03ab489955423c743bda5af400f
KBelova <bcasspus@gmail.com>
2020-03-04 14:26:37 +0300

    задание 11 - попап

--- test_project/js/script.js
+++ test_project/js/script.js
@@ -103,6 +103,24 @@ window.addEventListener('DOMContentLoaded', function() { // событие ср�
 	}
 
 	setClock('timer', deadLine);
+
+	// Modal
+	let more = document.querySelector('.more'),
+		overlay = document.querySelector('.overlay'),
+		close = document.querySelector('.popup-close');
+
+	more.addEventListener('click', function() {
+		overlay.style.display = 'block';
+		this.classList.add('more-splash');
+		document.body.style.overflow = 'hidden';
+	});
+
+	close.addEventListener('click', function() {
+		overlay.style.display = 'none';
+		more.classList.remove('more-splash');
+		document.body.style.overflow = '';
+	});
+
 });
 
 
