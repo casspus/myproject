dcc884e1264f67777c90a24ed0056ea41381ddfa
KBelova <bcasspus@gmail.com>
2020-03-10 09:42:34 +0300

    lesson 12

--- script.js
+++ script.js
@@ -1,132 +1,29 @@
 'use strict';
 
-let money, time;
-
-function start(){
-	money = +prompt("Ваш бюджет на месяц ");
-	time = prompt("Введите дату в формате YYYY-MM-DD", "");
-
-	while(isNaN(money) || money == "" || money == null) {
-		money = +prompt("Ваш бюджет на месяц ");
+// Создать класс options
+class Option {
+	// Он должен содержать свойства: height, width, bg, fontSize, textAlign
+	constructor(height, width, color, bg, fontSize, textAlign){
+		this.height = height,
+		this.width = width,
+		this.color = color,
+		this.bg = bg,
+		this.fontSize = fontSize,
+		this.textAlign = textAlign;
 	}
-};
-start();
-    
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: true,
-
-    //метод объекта appData
-    chooseExpenses: function() {
-		for (let i = 0; i < 2; i++){
-			let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-				b = prompt("Во сколько обойдется", "");
-			if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-				console.log("done");
-				appData.expenses[a] = b;
-			} else {
-				i = i - 1;
-			}
-		};    	
-    },
-
-    //метод объекта appData
-    detectDayBudget: function() {
-		appData.moneyPerDay = (appData.budget / 30).toFixed();	
-		alert("Ваш дневной бюджет: " + appData.moneyPerDay + "руб.");    	
-    },
-
-    //метод объекта appData
-	detectLevel: function() {
-		if(appData.moneyPerDay < 1000){
-			console.log("Уровень достатка низкий");
-		} else if(appData.moneyPerDay > 1000 && appData.moneyPerDay < 3000){
-			console.log("Уровень достатка средний");
-		} else if (appData.moneyPerDay > 3000){
-			console.log("Уровень достатка высокий");
-		} else {
-			console.log("Ошибка");
-		}		
-	},
-
-    //метод объекта appData
-	checkSavings: function() {
-		// какова сумма накоплений и под какой процент
-		if(appData.savings == true){
-			let save = +prompt("Какова сумма накоплений?"),
-				percent	= +prompt("Под какой процент?");
-
-			//прибыль за месяц:	
-			//деньги делим на 100/12 месяцев*на процент, который указал пользователь
-			appData.monthIncome = save/100/12*percent;
-			alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
-		}
-	},
-
-    //метод объекта appData
-	chooseOptExpenses: function() {
-		// Функция для определения необязательных расходов
-		for (let i = 1; i <= 3; i++){
-			let questionOptExpenses = prompt("Статья необязательных расходов?");	
-			appData.optionalExpenses[i] = questionOptExpenses;
-		}	
-		console.log(appData.optionalExpenses);		
-	},
-
-	//метод объекта appData
-	chooseIncome: function() {
-		let items = prompt("Что принесет доп. доход? (Перечислите через запятую)" , "");
-
-		if (typeof(items) != "string" || items == "" || typeof(items) == null){
-
-			console.log("Вы ввели некорректные данные или не ввели их вовсе");
-
-		} else {
-			appData.income = items.split(', ');
-
-			//добавить какой-то элемент в конец
-			appData.income.push(prompt("Может что-то еще?"));
-
-			//сортируем по алф.
-			appData.income.sort();					
-		}	
-
-		appData.income.forEach (function (itemmassive, i) {
-			alert("Способы доп. заработка: " + (i+1) + " - " + itemmassive);
-		});	
-
+	// Он должен содержать метод, создающий новый div на странице, записывающий в него любой текст и при помощи cssText изменять свой стиль из переданных параметров
+	createDiv() {
+		let elem = document.createElement('div');	
+		let text = document.createTextNode('Какой-то текст');	
+		document.body.appendChild(elem);
+		elem.appendChild(text);
+		let param = `height: ${this.height}px; width: ${this.width}px; color: ${this.color}; background: ${this.bg}; font-size: ${this.fontSize}px; text-align: ${this.textAlign}`;
+		elem.style.cssText = param;
 	}
-};	
-
-//запускае методы
-appData.detectDayBudget();		
-appData.detectLevel();		
-appData.chooseIncome();		
-
-for (let key in appData) {
-    console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
 }
 
+// Создать новый объект через класс
+const item = new Option(100, 300, '#333', 'yellow', 18, 'left');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Вызвать его метод и получить элемент на странице
+item.createDiv();
